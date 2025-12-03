ada65bcc5ed6ab591c8a879d067d555c0db21f94
SP-JULIO <jccekalle@gmail.com>
2025-12-03 01:51:51 -0500

    Ej3.py

--- Ej3.py
+++ Ej3.py
@@ -1,19 +1,21 @@
-# Solicitud de entrada de datos
-contraseña_1 = input("Crea una nueva contraseña: ")
-contraseña_2 = input("Confirma tu contraseña: ")
+#Solicitud de datos
+contraseña_1 =input("Solicitud crear contraseña". )
+contraseña_2 =input("confirmar contraseña". )
 
-# Función para validar reglas de la contraseña
-def validar_contrasena(contrasena):
-    if len(contrasena) < 8:
-        return "¡Error! La contraseña debe tener al menos 8 caracteres."
-    if not any(r"[A-Z]", contrasena):
-        return "¡Error! La contraseña debe contener al menos una letra mayúscula."
-    if not any(r"[a-z]", contrasena):
-        return "¡Error! La contraseña debe contener al menos una letra minúscula."
-    if not any(r"[0-9]", contrasena):
-        return "¡Error! La contraseña debe contener al menos un número."
-    if not any(r"[!@#$%^&*(),.?\":{}|<>]", contrasena):
-        return "¡Error! La contraseña debe contener al menos un carácter especial."
-    return "OK"
+# Verificacion de datos
+if (contraseña_1 == contraseña_2
+   and len(contraseña_1) >=8
+   and any(c.isupper() for c in contraseña_1)
+   and any(c.islower() for c in contraseña_1)
+   and any(c.isdigit() for c in contraseña_1)
+   and any(c.in"!#$%&/?¡¿÷×-+~_" for c in contraseña_1)
+   print(" La contraseña se ha creado exitosamente")
 
-# fin del del codigo
+elif len(contraseña_1) < 8 :
+    print("!Error¡ La contraseña debe de tener al menos 8 caracteres")
+
+else
+   # si no son iguales y la longitud es >=8, este es el unico caso restante
+   print("!Error¡ Las contraseñas no cumplen con los requisitos deben de tener al menos mayusculas, minusculas, numeros y caracteres")
+
+# fin del codigo
